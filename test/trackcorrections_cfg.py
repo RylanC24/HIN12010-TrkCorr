ab7a7e1378de419cff760e8fbfaacddf794cc03b
Rylan <RylanC24@gmail.com>
2014-10-14 14:34:14 -0400

    got the code "working" in PbPb, I just need to find the right dataset and GT

--- test/trackcorrections_cfg.py
+++ test/trackcorrections_cfg.py
@@ -3,24 +3,29 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process('TRACKANA')
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-#process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#process.load('Configuration.EventContent.EventContentHeavyIons_cff')
+#process.GlobalTag.globaltag = 'STARTHI53_V27::All'
+#process.GlobalTag.globaltag = 'GR_R_53_LV6::All'
+process.GlobalTag.globaltag = 'STARTHI53_LV1::All'
+
 process.load('Appeltel.HIN12010_TrkCorr.RpPbTrackingCorrections_cfi')
 process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('trackCorrections.root')
 )
+
 
 from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import *
 overrideCentrality(process)
@@ -30,13 +35,11 @@ process.HeavyIonGlobalParameters = cms.PSet(
 #  nonDefaultGlauberModel = cms.string(""),
   nonDefaultGlauberModel = cms.string("Hydjet_Drum"),
   centralitySrc = cms.InputTag("hiCentrality"),
-  pPbRunFlip = cms.untracked.uint32()
   )
 
 process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
-
 process.tpRecoAssocGeneralTracks = process.trackingParticleRecoTrackAsssociation.clone()
-process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("generalTracks")
+process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("hiGeneralTracks")
 
 process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
 process.TrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
@@ -45,32 +48,21 @@ process.TrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
 process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
     fileNames =  cms.untracked.vstring(
-'/store/user/vzhukova/HIJING_GEN-SIM_YUE-SHI_Minbias_2_v1/HIJING_RECO_YUE-SHI_Minbias__2_v1/b7d33bba7673cdb1ee6f4983c0800c79/hijing_reco_fix_9_2_wnY.root'
+'/store/himc/HiFall13DR53X/Pyquen_Unquenched_AllQCDPhoton170_PhotonFilter35GeV_eta3_TuneZ2_Hydjet1p8_2760GeV/GEN-SIM-RECO/NoPileUp_STARTHI53_LV1-v1/30000/00018A42-C621-E411-B8FD-848F69FD4EB6.root'
     )
 )
 
-#process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
-#process.hltSingleTrigger = process.hltHighLevel.clone()
-#process.hltSingleTrigger.HLTPaths = ["HLT_PAZeroBiasPixel_SingleTrack_v1"]
+process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
+process.hltMinBiasHFOrBSC= process.hltHighLevel.clone()
+process.hltMinBiasHFOrBSC.HLTPaths = ["HLT_PAZeroBiasPixel_SingleTrack_v1"]
 
-#process.GlobalTag.globaltag = 'STARTHI53_V27::All'
-#process.GlobalTag.globaltag = 'GR_R_53_LV6::All'
-process.GlobalTag.globaltag = '???::All'
 
 process.p = cms.Path(
-			siPixelRecHits *
+			process.siPixelRecHits *
 			process.hltMinBiasHFOrBSC * 
 			process.collisionEventSelection *
 			process.tpRecoAssocGeneralTracks *
 			process.pPbTrkCorr
 )			 
 
-#process.p = cms.Path( 
-#                      process.siPixelRecHits *
-#                      process.hltMinBiasHFOrBSC *
-#		      process.collisionEventSelection *
-#		      process.pACentrality *
-#                      process.tpRecoAssocGeneralTracks *
-#                      process.pPbTrkCorr 
-#)
 
