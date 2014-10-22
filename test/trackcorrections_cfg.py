ad37d4013fc8100940511781aa67dbc57bfa4b4d
Rylan <RylanC24@gmail.com>
2014-10-21 20:37:56 -0400

    removed minbias HLT path and modified vertex and track sources to HI specific object

--- test/trackcorrections_cfg.py
+++ test/trackcorrections_cfg.py
@@ -12,7 +12,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = 'GR_R_53_LV6::All'
 process.GlobalTag.globaltag = 'STARTHI53_LV1::All'
 
-process.load('Appeltel.HIN12010_TrkCorr.RpPbTrackingCorrections_cfi')
+process.load('Appeltel.HIN12010_TrkCorr.PbPbTrackingCorrections_cfi')
 process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
 
 process.maxEvents = cms.untracked.PSet(
@@ -23,7 +23,8 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('trackCorrections.root')
+#    fileName = cms.string('trackCorrections.root')
+    fileName = cms.string('/net/hisrv0001/home/rconway/HIN_12_010/TrkCorr/CMSSW_5_3_20/src/Appeltel/HIN12010_TrkCorr/test/output/trackCorrections.root')
 )
 
 
@@ -48,18 +49,18 @@ process.TrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
 process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
     fileNames =  cms.untracked.vstring(
-'/store/himc/HiFall13DR53X/Pyquen_Unquenched_AllQCDPhoton170_PhotonFilter35GeV_eta3_TuneZ2_Hydjet1p8_2760GeV/GEN-SIM-RECO/NoPileUp_STARTHI53_LV1-v1/30000/00018A42-C621-E411-B8FD-848F69FD4EB6.root'
+'/store/himc/HiFall13DR53X/Pyquen_DiJet_Pt120_TuneZ2_Unquenched_Hydjet1p8_2760GeV/GEN-SIM-RECO/NoPileUp_STARTHI53_LV1-v3/00000/0010470D-A6DD-E311-99E2-00266CF9AE10.root'
     )
 )
 
-process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
-process.hltMinBiasHFOrBSC= process.hltHighLevel.clone()
-process.hltMinBiasHFOrBSC.HLTPaths = ["HLT_PAZeroBiasPixel_SingleTrack_v1"]
+#process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
+#process.hltMinBiasHFOrBSC= process.hltHighLevel.clone()
+#process.hltMinBiasHFOrBSC.HLTPaths = ["HLT_HIMinBiasHfOrBSC_v1"]
 
 
 process.p = cms.Path(
 			process.siPixelRecHits *
-			process.hltMinBiasHFOrBSC * 
+#            process.hltMinBiasHFOrBSC * 
 			process.collisionEventSelection *
 			process.tpRecoAssocGeneralTracks *
 			process.pPbTrkCorr
