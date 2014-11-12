dff86baadf405ff96df58009e0059d9ca3e76de6
Rylan <RylanC24@gmail.com>
2014-11-12 15:20:38 -0500

    fixed a missing comma causing a compilation error

--- python/PbPbTrackingCorrections_cff.py
+++ python/PbPbTrackingCorrections_cff.py
@@ -4,7 +4,7 @@ import Appeltel.HIN12010_TrkCorr.PbPbTrackingCorrections_cfi
 
 trkCorr_HIN12017 = Appeltel.RpPbAnalysis.PbPbTrackingCorrections_cfi.PbPbTrkCorr.clone(
 #    vertexSrc = cms.InputTag("offlinePrimaryVertices"),
-    vertexSrc = cms.InputTag("hiSelectedVertex")
+    vertexSrc = cms.InputTag("hiSelectedVertex"),
     ptBins = cms.vdouble(
         0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
         0.5, 0.55,  0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0,  
